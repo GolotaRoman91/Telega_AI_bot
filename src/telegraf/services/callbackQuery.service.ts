740acb380da0319800daabf86e0285240d733468
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-04-23 10:07:25 +0300

    callbackQuery.service refactoring: main method separated to start and end conversationHandlers

--- src/telegraf/services/callbackQuery.service.ts
+++ src/telegraf/services/callbackQuery.service.ts
@@ -9,27 +9,44 @@ import {
 @Injectable()
 export class CallbackQueryService {
   constructor(private conversationService: ConversationService) {}
+
   handleCallbackQuery(ctx: Context, userStartedConversation: Set<number>) {
     if ('data' in ctx.callbackQuery) {
       const data = ctx.callbackQuery.data;
       const userId = ctx.callbackQuery.from.id;
 
       if (data === 'start_conversation') {
-        userStartedConversation.add(userId);
-        this.conversationService.startConversation(userId);
-        ctx.reply(
-          'You have started a new conversation. You can now send messages.',
-          endConversationKeyboard,
-        );
+        this.startConversationHandler(ctx, userId, userStartedConversation);
       } else if (data === 'end_conversation') {
-        userStartedConversation.delete(userId);
-        ctx.reply(
-          'Conversation has ended. Please select an action to proceed.',
-          startConversationKeyboard,
-        );
+        this.endConversationHandler(ctx, userId, userStartedConversation);
       }
 
       ctx.answerCbQuery();
     }
+  }
+
+  private startConversationHandler(
+    ctx: Context,
+    userId: number,
+    userStartedConversation: Set<number>,
+  ) {
+    userStartedConversation.add(userId);
+    this.conversationService.startConversation(userId);
+    ctx.reply(
+      'You have started a new conversation. You can now send messages.',
+      endConversationKeyboard,
+    );
+  }
+
+  private endConversationHandler(
+    ctx: Context,
+    userId: number,
+    userStartedConversation: Set<number>,
+  ) {
+    userStartedConversation.delete(userId);
+    ctx.reply(
+      'Conversation has ended. Please select an action to proceed.',
+      startConversationKeyboard,
+    );
   }
 }
