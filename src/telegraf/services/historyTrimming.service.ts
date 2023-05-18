683632a8adc8d6cafa425e4a3210907854265076
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-05-18 09:39:52 +0300

    Approved HistoryTrimmingService, now it work correct with all lenguages

--- src/telegraf/services/historyTrimming.service.ts
+++ src/telegraf/services/historyTrimming.service.ts
@@ -7,12 +7,20 @@ export class HistoryTrimmingService {
     maxCharacters: number,
   ) {
     let totalCharacters = history.reduce((total, message) => {
-      return total + message.content.length;
+      const englishLetters = (message.content.match(/[a-zA-Z]/g) || []).length;
+      const nonEnglishLetters = (message.content.match(/[^\x00-\x7F]/g) || [])
+        .length;
+      return total + englishLetters + nonEnglishLetters * 3.1;
     }, 0);
 
     while (totalCharacters > maxCharacters) {
       const removedMessage = history.shift();
-      totalCharacters -= removedMessage.content.length;
+      const englishLetters = (removedMessage.content.match(/[a-zA-Z]/g) || [])
+        .length;
+      const nonEnglishLetters = (
+        removedMessage.content.match(/[^\x00-\x7F]/g) || []
+      ).length;
+      totalCharacters -= englishLetters + nonEnglishLetters * 3.1;
     }
 
     console.log('---------------------------------------');
